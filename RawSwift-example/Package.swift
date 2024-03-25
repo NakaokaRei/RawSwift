0720d97f7c0c0988315b02cb1c20683fe9e954d8
NakaokaRei <reideeplearning@gmail.com>
2024-03-25 22:17:47 +0900

    feat: add example project

---
+++ RawSwift-example/Package.swift
@@ -0,0 +1,5 @@
+// This `Package.swift` is dummy file for RawSwift example.`
+
+import PackageDescription
+
+let package = Package()
---
+++ RawSwift-example/RawSwift-example/RawSwift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawSwift_exampleApp.swift
+//  RawSwift-example
+//
+//  Created by NakaokaRei on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RawSwift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
